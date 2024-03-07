72a3b620f7fc5532af176f6c04be4e975b786879
AzinY <54357413+AzinY@users.noreply.github.com>
2024-03-07 13:08:07 +0000

    Minor fix

--- pages/Themes_Elections_app.py
+++ pages/Themes_Elections_app.py
@@ -1,10 +1,14 @@
 import streamlit as st
+import os
+import sys
+
 #from spt_preproc import PreprocText
 #tp = PreprocText()
 
 #from sentence_transformers import SentenceTransformer
 #encoder = SentenceTransformer('intfloat/multilingual-e5-large')
-
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..','sub_modules', 'Themes_Elections')))
+ 
 from Themes_Elections import Elections_theme_classify
 
 st.title('Elections predictor')
